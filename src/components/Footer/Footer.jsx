53b571ab19e3e2537e6ff6578cb8b736d9f73c7f
unknown <np03cs4a220056@heraldcollege.edu.np>
2024-04-01 20:23:41 +0545

    ladning page

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Footer.css';
+import logo from '../ImagesFol/logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="footer-col">
+            <img src={logo} alt="" className='footerlogo'/>
+              <p className='textvroom'>
+                Vroom Vroom is a car rental website at your service. 
+                <p>
+                Choose us for the best cars and best vehicles.
+                </p>
+              </p>
+            </div>
+            <div className="footer-col quick-links">
+              <h4 id='text'>Quick Links</h4>
+              <ul id='links'>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
+            <div className="footer-col follow-us">
+              <h4 className='fol'>Follow Us</h4>
+              <div className="social-links">
+                <a href="#"><FontAwesomeIcon icon={faFacebookF} /></a>
+                <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
+                <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
+                <a href="#"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
